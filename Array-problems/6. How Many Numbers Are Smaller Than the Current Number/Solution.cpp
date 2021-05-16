1bd31519f32ca934d6dd213a12a6f054c8df5f16
Ajeet Patel <54282815+Ajeet1606@users.noreply.github.com>
2021-05-16 07:30:36 +0530

    Add files via upload

---
+++ Array-problems/6. How Many Numbers Are Smaller Than the Current Number/Solution.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> smallerNumbersThanCurrent(vector<int>& nums) {
+        std::vector<int> arr;
+	    for (int i = 0; i < nums.size(); ++i)
+	    {
+		int cnt = 0;
+		for (int j = 0; j < nums.size(); ++j)
+		{
+			if (nums[j]<nums[i])
+			{
+				cnt++;
+			}
+		}
+		arr.push_back(cnt);
+	    }
+        return arr;
+    }
+};
+TIME COMPLEXITY: O(n^2)
